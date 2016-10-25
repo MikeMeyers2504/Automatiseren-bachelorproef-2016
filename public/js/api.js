ce46cff026d7a7bb9b21645a722bde6e208f7b4b
MikeMeyers2504 <mike_sloefke@hotmail.com>
2016-10-25 11:20:12 +0200

    Files samenvoegen

--- public/js/api.js
+++ public/js/api.js
@@ -1,56 +1,90 @@
-var app = angular.module('app', []);
-
-function Login(args) {
-    this.login = args.login;
-}
+var app = angular.module('myApp');
 
 app.controller('MainCtrl', function($scope, $http) {
     var vm = this;
-    console.log("Hello");
-    alert('lol');
     
     vm.fetchData = function(user){
         vm.loading = true;
         $http.get('https://api.github.com/users/' + user.name).then(function(res){
                 console.log(res);
-                vm.name = res.data.name || "Not found";
+                vm.username = res.data.name || "Not found";
                 vm.login = res.data.login || "Not found";
-                //vm.data = res;
                 vm.loading = false;
-
         })
     }
 
-    /*vm.ShowData = function(users){
-        vm.loading = true;
+    vm.ShowData = function(){
         var i;
-        $http.get('https://api.github.com/orgs/AP-Elektronica-ICT/members?access_token=03d9996fa8c3a2e4b23efaa89901beeaf2dfd912').then(function(res){
-                var student = [];
+        $http.get('https://api.github.com/orgs/AP-Elektronica-ICT/members?access_token=74d60864cddbcbaccee4243da98771213cf29b36&page=1&per_page=100&role=member').then(function(res){ 
+                vm.studentNames = [];
                 for (i = 0; i < res.data.length; i++) {
-                	/*student.push({
-                		login: res.data[i]
-                	});*/
-    				/*console.log([res.data[i]]);
-    				//console.log(login);
-    				vm.login = res.data[i].login || "Not found";
-    				vm.loading = false;
-				}
-				return login;
-        })
-    }*/
-
-    vm.ShowData = function(usersNames){
-        $http.get('https://api.github.com/orgs/AP-Elektronica-ICT/members?access_token=03d9996fa8c3a2e4b23efaa89901beeaf2dfd912').then(function(res){
-                var studentNames = [];
-                for (i = 0; i < res.data.length; i++) {
-                    studentNames.push({
-                        login: res.data[i]
-                    });
-                    console.log([res.data[i]]);
-                    vm.login = res.data[i].login || "Not found";
+                    vm.studentNames.push(
+                        res.data[i].login
+                    );
+                    vm.login = vm.studentNames || "Not found";
                 }
-                return login;
+                console.log(vm.studentNames);
+        })
+    }
 
+    vm.ShowData();
+
+    vm.GetRepos = function(userLogin){
+        var found = false;
+        vm.loading = true;
+        $http.get('https://api.github.com/users/' + userLogin + '/repos').then(function(res){
+                console.log(res);
+                for (var i =  0; i < res.data.length; i++) {
+                    if (res.data[i].name.indexOf("2EALOVESDOGGEN")!== -1) { // Hij geeft de index waar de gezochte string begint, vind hij niks dan geeft hij -1
+                        console.log(res.data[i].name);
+                        console.log(res.data[i].id);
+                        console.log(res.data[i].open_issues);
+                        console.log(res.data[i].owner.login);
+                        vm.sha = null;
+                        vm.message = null;
+                        vm.name = res.data[i].name;
+                        vm.id = res.data[i].id;
+                        vm.open_issues = res.data[i].open_issues;
+                        vm.owner = res.data[i].owner.login;
+                        found = true;
+                    } 
+                }
+                if (found == false) {
+                    vm.name = null;
+                    vm.id = null;
+                    vm.open_issues = null;
+                    vm.owner = null;
+                    vm.sha = null;
+                    vm.message = null;
+                    alert('No BAP repo found');
+                    console.log(found);
+                }
+                vm.loading = false;
+        })
+    }
+
+    vm.GetCommits = function(userLogin, repoName){
+        var found = false;
+        vm.loading = true;
+        $http.get('https://api.github.com/repos/' + userLogin + "/" + repoName + '/commits').then(function(res){
+                vm.commits = [];
+                vm.messageNames = [];
+                console.log(res);
+                for (var i =  0; i < res.data.length; i++) {
+                    vm.commits.push(
+                        res.data[i].sha
+                    );
+                    vm.sha = vm.commits || "Not found";
+
+                    vm.messageNames.push(
+                        res.data[i].commit.message
+                    );
+                    vm.message = vm.messageNames || "Not found";
+                    found = true;
+                }
+                vm.loading = false;
+                console.log(vm.sha);
+                console.log(vm.message);
         })
     }
 });
